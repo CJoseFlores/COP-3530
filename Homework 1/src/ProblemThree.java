0660713b2b19515eaa5e70862de320583fe6342d
Carlos Flores <cflor083@fiu.edu>
2018-01-23 19:59:10 -0500

    Changed data type to BigInteger, and converted the algo to Matrix Exponentiation. Also included explanation and comments.

--- Homework 1/src/ProblemThree.java
+++ Homework 1/src/ProblemThree.java
@@ -1,70 +1,75 @@
 /*******************************************************************************
- * Purpose/Description: PLEASE FILL
+ * Purpose/Description: A program that recursively finds the "nth" number in
+ * the fibonacci sequence F(n), with time complexity O(log n)
  * Author's Panther ID: 5160328
  * Certification:
     I hereby certify that this work is my own and none of it is the work of any
     other person.
  * QUESTION (b): The algorithm implemented is of complexity O(log n)
- * The first if statement simply has a return statement, so it has constant 
- * time complexity.
- * The while loop contains an if statement with constant time complexity, 
- * and a counter that is halved on every iteration.
- * Since everything inside the while loop has constant time complexity, then
- * the while loop's complexity only depends on how the amount of iterations depends
- * on the input size.
- * Lets assume that "k" is the amount of iterations, and "n-1" is the input size
- * (since i = n-1 in the beginning, but lets assume the input size is "n" for simplicity).
- * Looking at the while loop, the counter "i" is halved on every iteration, so
- * lets observe the number of iterations if n >= 2 and is a power of 2.
- * Example: n = 8 = 2^3
- * First iteration, i = 8, second iteration i = 4, third iteration i = 2, fourth iteration = 1
- * And then finally when the fourth iteration is over, i = 0 so the while loop ends.
- * We can see that for 2^3, there are 4 iterations.
- * We can then see that the relationship for "k" iterations with input size "n"
- * is simply n = 2^(k-1) when n >= 2 that is a power of 2. To actually get the number of iterations,
- * we solve for "k" and get k = (log n) + 1. 
- * Note that for n >= 2 that is a power of 2, this equation also applies, except
- * the log is floored, i.e. k = floor(log n) + 1.
- * Ex: n = 9, then the loop runs k = floor(log 9) + 1 = floor(3.16) + 1 
- * = 3 + 1 = 4 times.
- * So we now know that T(n) is roughly floor(log n) + 1, which translates to
- * O(log n). (not that although T(n) is only valid for n >= 2, time complexity
- * is only concerned with n -> infinity, so this is still fine).
- * References: https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Fibonacci_Number_Program
+ * The method that actually implements the algorithm is "the BigInteger()" method.
+ * Following the link from the references, two formulas were derived from
+ * matrix exponentiation, one for even "n" and one for odd "n"
+ * For even n: F(2n) = (2F(n-1) + F(n))* F(n)
+ * For odd n: F(2n - 1) = F(n)^2 + F(n-1) ^2
+ * Looking at these equations, we note that we are not getting F(n), so we simply
+ * plug in "n/2" for "n" in the even equation, and "(n+1)/2" for "n" in the odd
+ * equation, which results in the following new equations:
+ * For even n: F(n) = (2F((n/2) -1) + F(n/2)) * F(n/2)
+ * For odd n: F(n) = F((n+1)/2)^2 + F(((n+1)/2) - 1)^2
+ * We can see that for every iteration, we are dividing the size by two.
+ * I.e., for every recursive method call, we are reducing the size by two,
+ * which according to the logarithm general rule, since we are halving the input
+ * size on every recursive call, we end up with complexity O(log n). There are also
+ * two trivial cases when n is 1 or 2, f(n) = 1, and when n is 0, f(n) = 0, which
+ * both simply use constant complexity of O(1) since it does not depend on the size.
+ * Therefore, the time complexity of the algorithm I implemented is O(log n).
+ * References: http://www.cs.utexas.edu/users/EWD/ewd06xx/EWD654.PDF
  * This reference was used to implement the fibonacci number sequence in sub-linear time.
 *******************************************************************************/
+import java.math.BigInteger;
+
 public class ProblemThree {
 
     // <--------------------------------------------------------------->
     // <-------THIS IS THE METHOD THAT IMPLEMENTS THE ALGORITHM!------->
     // <--------------------------------------------------------------->
-    // Generates the fibonacci number through exponentiation by squaring.
-    private static double fibNum(int n)
+    // Generates the fibonacci number through formulas derived from 
+    //exponentiation by matricies.
+    private static BigInteger fib(int n)
     {
-        // Variables used to exponentiate the numbers, and generate the fib number.
-        double i = n - 1, a = 1, b = 0, c = 0, d = 1, t;
-        
-        //fibNum(0) is simply 0. Also return 0 for negative numbers.
-        if(n <= 0 )
+        BigInteger ans; // Retains the value fib(n)
+
+        // Trivial case of fib(2) == fib(1) == 0
+        if(n == 2 || n == 1)
         {
-            return 0;
+            ans = BigInteger.valueOf(1);
+        }
+        // Trivial case of fib(0) = 0
+        else if(n == 0)
+        {
+            ans = BigInteger.valueOf(0);
+        }
+        //Use the f(2n) formula for even values.
+        else if(n % 2 == 0)
+        {
+            //The equation for even size is F(2n) = (2F(n-1) + F(n))* Fn
+            //We can therefore reduce the size by 2 on each iteration by plugging
+            //in "n/2" for n, thereby deriving F(n)
+            //i.e. F(n) = (2F((n/2) -1) + F(n/2)) * F(n/2)
+            ans = fib((n/2) - 1).multiply(BigInteger.valueOf(2))
+                    .add(fib(n/2)).multiply(fib(n/2));
+        }
+        //Use the f(2n - 1) formula for odd values.
+        else
+        {
+            //The same f(n) can be derived for odd values.
+            //Original Equation for Odd size: F(2n - 1) = F(n)^2 + F(n-1) ^2
+            //Here, we can plug in (n+1)/2 for "n" to get F(n)
+            //i.e. F(n) = F((n+1)/2)^2 + F(((n+1)/2) - 1)^2
+            ans = fib((n+1)/2).pow(2).add(fib(((n+1)/2) - 1).pow(2));
         }
         
-        while(i > 0)
-        {
-            if (i % 2 == 1)
-            {
-                t = d*(b + a) + c*b;
-                a = d*b + c*a;
-                b = t;
-            }
-            t = d*(2*c + d);
-            c = c*c + d*d;
-            d = t;
-            i = i / 2;
-            i = Math.floor(i); //Truncate decimal places to simulate int division.
-        }
-        return a + b;
+        return ans;
     }
     
     /**
@@ -75,10 +80,10 @@ public class ProblemThree {
         //Print out fibNum(0) through fibNum(10)
         for(int i=0; i <= 10; i++)
         {
-            System.out.println("fibNum(" + i + ") = " + fibNum(i));
+            System.out.println("fibNum(" + i + ") = " + fib(i));
         }
         
-        System.out.println(fibNum(250));
+        System.out.println(fib(250));
     }
     
 }
