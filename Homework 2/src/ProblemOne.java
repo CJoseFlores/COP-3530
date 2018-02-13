133bf89ffa03cca73897b1e06640f44b0ebfecdf
Carlos Flores <cflor083@fiu.edu>
2018-02-13 00:54:44 -0500

    Got Problem 1 to work.

--- Homework 2/src/ProblemOne.java
+++ Homework 2/src/ProblemOne.java
@@ -1,21 +1,99 @@
 /*********************************************************************
- Purpose/Description: <a brief description of the program>
+ Purpose/Description: Searches an array by dividing the array by 3, and recursively
+ * calling itself to search one of the three sectors until element is found or
+ * it is determined the element does not exist in the array.
  Author’s Panther ID: 5160328
  Certification:
  I hereby certify that this work is my own and none of it is the work of
  any other person.
+ * Question (B):
+ * The time complexity of the method "terSearch()" is O(log N). On every 
+ * recursive call, the input size (array) is divided by 3 by shifting either
+ * "l" or "r" or both to one of the sectors defined by "d1" and "d2". Using the
+ * logarithmic rule, then the complexity is log3(N), which is simply O(log N).
  ********************************************************************/ 
 
-/**
- *
- */
 public class ProblemOne {
 
+    private static int terSearch(int arr[], int l, int r, int x)
+    {
+        // Two pivot points used to divide array in three sectors.
+        int d1 = l + (r-l)/3;
+        int d2 = d1 + (r-l)/3;
+
+        // If l > r, then the term was not found in the array.
+        if(l > r)
+        {
+            return -1;
+        }
+        // if arr[d1] == x, we found the element, return the index "d1".
+        else if(arr[d1] == x)
+        {
+            return d1;
+        }
+        // if arr[d2] == x, we found the element, return the index "d2".
+        else if(arr[d2] == x)
+        {
+            return d2;
+        }
+        // if arr[d1] > x, x must be in the first sector, move r to "d1 - 1".
+        else if(arr[d1] > x)
+        {
+            return terSearch(arr, l, d1 - 1, x);
+        }
+        // if arr[d2] < x, x must be in the last sector, move l to "d2 + 1".
+        else if(arr[d2] < x)
+        {
+            return terSearch(arr, d2 + 1, r, x);
+        }
+        // Otherwise, x must be in the middle circle, move l & r to d1 & d2 
+        // respectively.
+        else
+        {
+            return terSearch(arr, d1, d2, x);
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int [] a = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int searchResult; // Holds result of the search.
+        int searchTermOne = 15; // Test to see if returns proper index.
+        int searchTermTwo = 22; // Test to see if returns -1.
+        
+        System.out.print("Given Array: [");
+        for(int i: a)
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println("]");
+        
+        // Testing if number can be properly found.
+        searchResult = terSearch(a, 0, a.length - 1, searchTermOne);
+        if(searchResult == -1)
+        {
+            System.out.println("Sorry! \"" + searchTermOne + "\" was not found!");
+        }
+        else
+        {
+            System.out.println("Nice! \"" + searchTermOne + "\" was found at index: "
+                    + searchResult);
+        }
+        
+        //Testing if term is not found.
+        searchResult = terSearch(a, 0, a.length - 1, searchTermTwo);
+        if(searchResult == -1)
+        {
+            System.out.println("Sorry! \"" + searchTermTwo + "\" was not found!");
+        }
+        else
+        {
+            System.out.println("Nice! \"" + searchTermTwo + "\" was found at index: "
+                    + searchResult);
+        }
+        
     }
     
 }
---
+++ d
@@ -0,0 +1,71 @@
+[33mcommit 92c89d4ab1354a14997cf1aaf38531b9cccabae5[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Mon Feb 12 21:32:00 2018 -0500
+
+    2b works at O(N * M)
+
+[33mcommit 16d89b3a213a194d7e9f486eaca04d8d3a9cc359[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Mon Feb 12 17:41:24 2018 -0500
+
+    Got 2a to work fully, using O(N*M).
+
+[33mcommit 743ccac4aae941f390e6b1a4657e28413c3954d7[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Mon Feb 12 17:20:47 2018 -0500
+
+    Got #2a working using O(N * M) but the last element does not get added to difference list.
+
+[33mcommit fd9b8ee3a44b200923b6f0b7786d77e48d3091b6[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Sun Feb 11 18:40:37 2018 -0500
+
+    Created a very basic generic link list.
+
+[33mcommit 38deeb063e04e2ab6cbbe87e0d15d7efdf87cea7[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Sun Feb 11 16:39:38 2018 -0500
+
+    Changed some comments
+
+[33mcommit d7d323677f8ad00bbe18937b1d9b342e3db14d10[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Wed Jan 24 11:28:32 2018 -0500
+
+    Edited comments.
+
+[33mcommit 0660713b2b19515eaa5e70862de320583fe6342d[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Tue Jan 23 19:59:10 2018 -0500
+
+    Changed data type to BigInteger, and converted the algo to Matrix Exponentiation. Also included explanation and comments.
+
+[33mcommit 6eeb7819dbfb44be4c904731897439f74d7f90f1[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Sun Jan 21 16:33:22 2018 -0500
+
+    HW1 - Fixed problem, but is now a double which doesn't comply with assignment requirements. Also gave answer for question B.
+
+[33mcommit 5f494fa69a1f5140f1c4b915f2bca826c98f3a2c[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Sun Jan 21 15:20:15 2018 -0500
+
+    HW1 - Implemented an O(log n) algo for fibonacci sequence, but cannot calculate fibNum(250) as the resulting size is beyond what 'long' can hold (signed 64-bit int).
+
+[33mcommit 6266bcb70866463bb269eec60bd23e80979286e9[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Sat Jan 20 18:40:04 2018 -0500
+
+    HW1 - Added additional comments.
+
+[33mcommit 923d6c5f7493db3edf859ff4a08e6acd23e5ffa0[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Sat Jan 20 18:36:54 2018 -0500
+
+    HW1 - Finished Problem Two.
+
+[33mcommit a72756f7c47b82015dd0a968ad76e0199e6d6090[m
+Author: Carlos Flores <cflor083@fiu.edu>
+Date:   Sat Jan 20 17:25:22 2018 -0500
+
+    HW1 - Finished Problem 1.
