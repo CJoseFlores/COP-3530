d487950e71d247fb63e738f680a0f117fef3f8b8
Carlos Flores <cflor083@fiu.edu>
2018-03-24 18:49:28 -0400

    Finished Problem 1, Part C

--- Homework-3/src/ProblemOne.java
+++ Homework-3/src/ProblemOne.java
@@ -4,6 +4,14 @@
  Certification:
  I hereby certify that this work is my own and none of it is the work of
  any other person.
+ * Questions:
+ 
+ REFERENCES:
+ * Note, the code for min/max heap implementation was taken from the following
+ * links (since it is assumed we have them available):
+ * http://users.cis.fiu.edu/~weiss/cop3530_sum08/July16.java
+ * https://users.cs.fiu.edu/~weiss/dsj4/code/weiss/nonstandard/UnderflowException.java
+ * The code was modified to work with the problem.
  ********************************************************************/ 
 
 import java.util.Comparator;
@@ -232,9 +240,11 @@ public class ProblemOne {
         {
             if( currentSize == heap.length - 1 )
                 enlargeArray( heap.length * 2 + 1 );
-
+            
+            currentSize++;
+            
             // Percolate up
-            percolateUp(x);
+            percolateUp(x, currentSize);
         }
 
 
@@ -309,11 +319,10 @@ public class ProblemOne {
             heap[ hole ] = tmp;
         }
         
-        
         //Who did this!?!? 😂👌
-        private void percolateUp( AnyType x)
+        private void percolateUp( AnyType x, int currentIndex)
         {
-            int hole = ++currentSize;
+            int hole = currentIndex;
             for( ; hole > 1 && myCompare(x, heap[ hole / 2 ] ) > 0; hole /= 2 )
                 heap[ hole ] = heap[ hole / 2 ];
             heap[ hole ] = x;
@@ -326,43 +335,102 @@ public class ProblemOne {
             return Arrays.toString(heap);
         }
         
+        /***************************************************** 
+        * THE CODE BELOW IS USED TO ANSWER PROBLEM 1, PART (C)
+        ***************************************************** 
+        */
+        
+        /**
+         * Replace an "oldKey" with a "newKey" if found, otherwise print error.
+         * @param oldKey The key to replace.
+         * @param newKey The key to replace with.
+         */
+        public void replaceKey(AnyType oldKey, AnyType newKey)
+        {
+            // Iterate through the used portion of the heap array, and replace
+            // oldKey with newKey if found, otherwise print response.
+            for(int i = 1; i < currentSize + 1; i++)
+            {
+                if(heap[i].equals(oldKey))
+                {
+                    heap[i] = newKey; // Replacing the oldKey.
+                    
+                    // Attempt to percolateUp if needed, otherwise it will 
+                    // exit on first attempt.
+                    percolateUp(newKey, i);
+                    
+                    // Attempt to percolateDown if needed, otherwise it will
+                    // exit on first attempt.
+                    percolateDown(i);
+                    
+                    return;
+                }
+            }
+            
+            System.out.println("The specified key is not in the heap.");
+        }
+        
     }
 
     // Test program
     public static void main( String [ ] args )
     {
-        ProblemOne.MinHeap<Integer> minHeapTest = new ProblemOne().new MinHeap<Integer>( );
+        //ProblemOne.MinHeap<Integer> minHeapTest = new ProblemOne().new MinHeap<Integer>( );
         ProblemOne.MaxHeap<Integer> maxHeapTest = new ProblemOne().new MaxHeap<Integer>( );
         
-        //Inserting heap from board.
-        minHeapTest.insert(10);
-        minHeapTest.insert(20);
-        minHeapTest.insert(30);
-        minHeapTest.insert(21);
-        minHeapTest.insert(22);
-        System.out.println("Min Heap: ");
-        System.out.println(minHeapTest);
-        System.out.println("Inserting 6...");
-        minHeapTest.insert(6);
-        System.out.println(minHeapTest);
-        System.out.println("Deleting Min..");
-        minHeapTest.deleteMin();
-        System.out.println(minHeapTest);
+//        //Inserting heap from board.
+//        minHeapTest.insert(10);
+//        minHeapTest.insert(20);
+//        minHeapTest.insert(30);
+//        minHeapTest.insert(21);
+//        minHeapTest.insert(22);
+//        System.out.println("Min Heap: ");
+//        System.out.println(minHeapTest);
+//        System.out.println("Inserting 6...");
+//        minHeapTest.insert(6);
+//        System.out.println(minHeapTest);
+//        System.out.println("Deleting Min..");
+//        minHeapTest.deleteMin();
+//        System.out.println(minHeapTest);
         
-        //Inserting heap from board.
-        maxHeapTest.insert(10);
-        maxHeapTest.insert(20);
-        maxHeapTest.insert(30);
-        maxHeapTest.insert(21);
-        maxHeapTest.insert(22);
-        System.out.println("Max Heap: ");
-        System.out.println(maxHeapTest);
-        System.out.println("Inserting 6...");
+        
+        
+        
+        
+        /***************************************************** 
+        * THE CODE BELOW IS USED TO ANSWER PROBLEM 1, PART (C)
+        ***************************************************** 
+        */
+        maxHeapTest.insert(99);
+        maxHeapTest.insert(64);
+        maxHeapTest.insert(42);
+        maxHeapTest.insert(54);
+        maxHeapTest.insert(32);
+        maxHeapTest.insert(28);
         maxHeapTest.insert(6);
+        maxHeapTest.insert(19);
+        maxHeapTest.insert(7);
+        maxHeapTest.insert(26);
+        maxHeapTest.insert(4);
+        System.out.println("--------------------------------------");
+        System.out.println("Problem 1, Part (C):");
+        System.out.println("--------------------------------------");
+        System.out.print("The following is the given heap: ");
         System.out.println(maxHeapTest);
-        System.out.println("Deleting Max...");
-        maxHeapTest.deleteMax();
+        System.out.println("Attempting to replace 54 with 105....");
+        maxHeapTest.replaceKey(new Integer(54), new Integer(105));
+        System.out.println("Resulting Heap: ");
         System.out.println(maxHeapTest);
+        System.out.println("\nAttempting to replace 100 with 200....");
+        maxHeapTest.replaceKey(new Integer(100), new Integer(200));
+        System.out.println("Resulting Heap: ");
+        System.out.println(maxHeapTest);
+        
+        
+        
+        
+        System.out.println("\nWho did this!?!? 😂👌");
+        
        
     }
 
